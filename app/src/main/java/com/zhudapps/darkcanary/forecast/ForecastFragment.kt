e83009adbb40fcf58ae179904340310d8b803990
Adrian Mohnacs <amohnacss@gmail.com>
2019-07-17 08:42:40 -0700

    tests included along with detailed dao

--- app/src/main/java/com/zhudapps/darkcanary/forecast/ForecastFragment.kt
+++ app/src/main/java/com/zhudapps/darkcanary/forecast/ForecastFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
@@ -53,6 +54,10 @@ class ForecastFragment : DaggerFragment() {
             arguments?.getInt(FORECAST_DAY_OFFSET)?.let {
                 //viewModel.daysToOffset = it
                 viewModel.getForecast(it)
+            }
+
+            details_button.setOnClickListener {
+                Toast.makeText(activity, "Under Construction", Toast.LENGTH_SHORT).show()
             }
 
             viewModel.forcastLiveData.observe(this, Observer {
